a5bf0a7b2532ffa4b55d779e80e917883252f27a
= <=>
2021-02-24 01:51:55 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [HomeController::class, 'home']);
+Route::get('/category', [HomeController::class, 'products']);
+Route::prefix('category')->group(function (){
+    Route::get('/marbel-edu-games',function (){
+        echo '<a href = "https://www.educastudio.com/category/marbel-edu-games">https://www.educastudio.com/category/marbel-edu-games</a>';
+    });
+    Route::get('/marbel-and-friends-kids-games',function (){
+        echo '<a href = "https://www.educastudio.com/category/marbel-and-friends-kids-games">https://www.educastudio.com/category/marbel-and-friends-kids-games</a>';
+    });
+    Route::get('/riri-story-books',function (){
+        echo '<a href = "https://www.educastudio.com/category/riri-story-books">https://www.educastudio.com/category/riri-story-books</a>';
+    });
+    Route::get('/kolak-kids-songs',function (){
+        echo '<a href = "https://www.educastudio.com/category/kolak-kids-songs">https://www.educastudio.com/category/kolak-kids-songs</a>';
+    });
+});
+Route::get('/berita', [HomeController::class, 'news']);
+Route::get('/news', function($p = 'news'){
+    echo '<a href = "https://www.educastudio.com/news">' .$p. '</a>';
+});
+Route::get('/news/{news}', function($p = 'educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19'){
+    echo '<a href = "https://www.educastudio.com/news/' .$p.'">'.$p. '</a>';
+});
+
+Route::get('/program', [HomeController::class, 'program']);
+Route::prefix('program')->group(function (){
+    Route::get('/karir',function (){
+        echo '<a href = "https://www.educastudio.com/program/karir">https://www.educastudio.com/program/karir</a>';
+    });
+    Route::get('/magang',function (){
+        echo '<a href = "https://www.educastudio.com/program/magang">https://www.educastudio.com/program/magang</a>';
+    });
+    Route::get('/kunjungan-industri',function (){
+        echo '<a href = "https://www.educastudio.com/program/kunjungan-industri">https://www.educastudio.com/program/kunjungan-industri</a>';
+    });
+});
+
+Route::get('aboutus',function (){
+    echo '<a href = "https://www.educastudio.com/about-us">https://www.educastudio.com/about-us</a>';
+});
+Route::get('/contactus', [ContactUsController::class, 'contactus']);
+
+
